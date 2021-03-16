fbf797e03921ddf4201fa8d1bb33d4b5a41ed0fa
Alexei Karyavin <alexis-karyavin@yandex.ru>
2021-03-16 23:50:18 +0300

    add points to map

--- src/models/map.ts
+++ src/models/map.ts
@@ -1,19 +1,43 @@
 import * as config from '../config.json'
-import mapboxgl from 'mapbox-gl'
+import mapboxgl, { Marker } from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 export default class Map {
-  public init (): void {
+  // TODO: Узнать интерфейс
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private map: any
+  private markers: Array<Marker>
+
+  constructor () {
     mapboxgl.accessToken = config.map.keyMap
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: 'map',
       style: config.map.style,
       center: [37.62876226496542, 55.750564371779575],
       zoom: config.map.zoom
     })
-    map.on('load', () => {
+
+    this.markers = []
+  }
+
+  public init (): void {
+    this.map.on('load', () => {
     // TODO: Here we want to load a layer
     // TODO: Here we want to load/setup the popup
+    })
+    // this.map.on('click', (e) => {
+    //   console.log(JSON.stringify(e.lngLat.wrap()))
+    // })
+    this.addMarkers()
+  }
+
+  private addMarkers (): void {
+    // Add points Marker
+    config.map.points.forEach(point => {
+      this.markers.push(new mapboxgl.Marker()
+        .setLngLat([point[0], point[1]])
+        .addTo(this.map)
+      )
     })
   }
 }
