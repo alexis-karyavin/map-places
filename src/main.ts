5897fff6da4fd25e4f596c6eda767bdb8b3ce328
Alexei Karyavin <alexis-karyavin@yandex.ru>
2021-03-16 22:59:08 +0300

    add primevue

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,16 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
 library.add(faUserSecret)
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(PrimeVue)
   .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
